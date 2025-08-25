097d602363f7f0870beb2449ac635f585271ca7f
Andre Caliento <andrecaliento@gmail.com>
2025-08-25 08:15:33 -0300

    Create exe005git.py

---
+++ exe005git.py
@@ -0,0 +1,20 @@
+#Exercício Python 5: Faça um programa que leia
+# um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+n = int(input('digite um numero:'))
+
+#print('analisando o numero:{}\n seu antecessor e {}\n e seu sucessor e:{}'.format(n,n-1,n+1))
+
+n1 = n -1
+n2 = n +1
+print(f'Analisando o numero {n}!')
+print(f'Seu antecessor é {n1}!')
+print(f'seu sucessor é {n2}!')
+
+Resposta:
+
+Digite um numero: 10
+Analisando o numero 10!
+Seu antecessor é 9!
+seu sucessor é 11!
+
