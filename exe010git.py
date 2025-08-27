b934c2bc8f03c81621a76bf66c6d18da5fc108ff
Andre Caliento <andrecaliento@gmail.com>
2025-08-27 14:31:37 -0300

    Create exe010git.py

---
+++ exe010git.py
@@ -0,0 +1,18 @@
+#Exercício Python 10: Crie um programa que leia quanto dinheiro
+# uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+
+d = float(input('Quanto dinheiro voce tem na carteira? R$ ' ))
+m = d / 3.27
+print(f'Com {d} reais voce consegue comprar {m:.2f} dolares.')
+
+print('~'*30)
+print('     Made by Andre Caliento')
+print('~'*30)
+
+
+Quanto dinheiro voce tem na carteira? R$ 100000
+Com 100000.0 reais voce consegue comprar 30581.04 dolares.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     Made by Andre Caliento
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
