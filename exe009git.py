331f52ee310ef6fca380de8ddf100f35ae83b492
Andre Caliento <andrecaliento@gmail.com>
2025-08-27 14:24:42 -0300

    Create exe009git.py

---
+++ exe009git.py
@@ -0,0 +1,36 @@
+#Exercício Python 9: Faça um programa que leia um número Inteiro
+# qualquer e mostre na tela a sua tabuada.
+
+n = int (input('Digite um numero para ver sua tabuada:'))
+print('-'*12)
+print('{} x {:2} ={}'.format(n,1,n*1))
+print('{} x {:2} ={}'.format(n,2,n*2))
+print('{} x {:2} ={}'.format(n,3,n*3))
+print('{} x {:2} ={}'.format(n,4,n*4))
+print('{} x {:2} ={}'.format(n,5,n*5))
+print('{} x {:2} ={}'.format(n,6,n*6))
+print('{} x {:2} ={}'.format(n,7,n*7))
+print('{} x {:2} ={}'.format(n,8,n*8))
+print('{} x {:2} ={}'.format(n,9,n*9))
+print('{} x {:2} ={}'.format(n,10,n*10))
+print('-'*12)
+print('~'*30)
+print('    Made by Andre Caliento')
+print('~'*30)
+
+Digite um numero para ver sua tabuada:10
+------------
+10 x  1 =10
+10 x  2 =20
+10 x  3 =30
+10 x  4 =40
+10 x  5 =50
+10 x  6 =60
+10 x  7 =70
+10 x  8 =80
+10 x  9 =90
+10 x 10 =100
+------------
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Made by Andre Caliento
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
