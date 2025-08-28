e8574431968d1f699e2c6dbdfde74b37f62018ac
Andre Caliento <andrecaliento@gmail.com>
2025-08-28 08:38:15 -0300

    Create exe012git.py

---
+++ exe012git.py
@@ -0,0 +1,25 @@
+print('~'*30)
+print('      MERCADO GUANABARA')
+print('~'*30)
+P = float(input('carne de frango:R$ '))
+d = float(input('Desconto % '))
+PD= P-(P*d/100)
+print(f'''Hoje a carne do frango esta com {d} % de desconto,
+preco final da carne de {P} R$ é de:{PD} R$''')
+
+print('~'*30)
+print('     Made by Andre Caliento')
+print('~'*30)
+
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      MERCADO GUANABARA
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+carne de frango:R$ 100
+Desconto % 20
+Hoje a carne do frango esta com 20.0 % de desconto,
+preco final da carne de 100.0 R$ é de:80.0 R$
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+     Made by Andre Caliento
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
