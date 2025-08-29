c99099475dccc10177b2fd176232c7921875e081
Andre Caliento <andrecaliento@gmail.com>
2025-08-29 08:02:22 -0300

    Create exe014git.py

---
+++ exe014git.py
@@ -0,0 +1,21 @@
+#Exercício Python 14: Escreva um programa que converta uma temperatura
+# digitando em graus Celsius e converta para graus Fahrenheit.
+
+print('~~'*15)
+print('Conversor de temperatura')
+print('~~'*15)
+t = float(input('temperatura em C:  '))
+f = 9 * t/5 +32
+print('Hoje a temperatura esta {}C e {}F.'.format(t,f))
+print('~~'*15)
+print('Made by Andre Caliento')
+print('~~'*15)
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   Conversor de temperatura
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+temperatura em C: 10
+Hoje a temperatura esta 10.0C e 50.0F.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Made by Andre Caliento
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
