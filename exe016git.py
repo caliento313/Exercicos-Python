6f50189ff3702fdb46bef3312610d9c07516049f
Andre Caliento <andrecaliento@gmail.com>
2025-09-01 08:57:02 -0300

    Create exe016git.py

---
+++ exe016git.py
@@ -0,0 +1,15 @@
+from math import trunc
+num = float(input('Digite um numero:').replace(',','.'))
+print(f'O valor digitado foi: {num} e sua parte inteira: {trunc(num)}')
+
+print('~~'*15)
+print('    Made by Andre Caliento')
+print('~~'*15)
+
+Digite um numero:3.333
+O valor digitado foi: 3.333 e sua parte inteira: 3
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Made by Andre Caliento
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
